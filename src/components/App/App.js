b687150a13f33336ed682cdbb5fc6bd6b3d520b3
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-07-15 23:51:04 +0530

    Add basic app structure and components

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../elements/Header/Header";
+import Home from "../Home/Home";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
